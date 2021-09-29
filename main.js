ff3f87b59be01faa7fe302dbff5b09d8d04dcac4
“Girum0” <gwa14@scarletmail.rutgers.edu>
2021-09-29 16:17:33 -0400

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+// function expression to select elements 
+const selectElement = (s) => document.querySelector(s);
+const navlinks = document.querySelectorAll(".nav-link");
+
+selectElement(".menu-icon").addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".menu-icon").classList.toggle("toggle")
+// animates the sidebar
+    navlinks.forEach((link,index) =>{
+        if(link.style.animation) {
+            link.style.animation=''
+         } else {
+ 
+             link.style.animation = `navLinkAnimate 0.5s ease forwards ${index /7 + 0.5}s`;
+         }
+         console.log(index / 7);
+     });
+} );
