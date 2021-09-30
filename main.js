0447e669a9a104a4e4820b0141d7458d77f8a621
“Girum0” <gwa14@scarletmail.rutgers.edu>
2021-09-29 20:08:37 -0400

    init

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ selectElement(".menu-icon").addEventListener("click", () => {
          console.log(index / 7);
      });
 } );
+
+navlinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+    selectElement(".menu-icon").classList.toggle("toggle");
+
+    navlinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkAnimate 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+        console.log(index / 7 + 0.5);
+      }
+    });
+  });
+});
